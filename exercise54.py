57ea99673b55e9a04721b7b1972cbb5ba04174e0
Alex Lau <aznalix@yahoo.com>
2014-06-19 19:06:25 -0400

    I think I finished this...

--- exercise54.py
+++ exercise54.py
@@ -1,26 +1,20 @@
 # This is where Exercise 5.4 goes
 # Name:Alex Lau
 
-
 def is_triangle(a, b, c):
-	if a > (b + c) or b > (a + c) or c > (a + b):
+	if a >= (b + c) or b >= (a + c) or c >= (a + b):
 		print 'No'
 	else:
 		print 'Yes'
 
 def prompt_lengths():
-	prompt = ''
+	prompt1 = 'Please input 1st length\n'
 	a = raw_input(prompt1)
-	prompt2 = 'Please input 2nd length'
+	prompt2 = 'Please input 2nd length\n'
 	b = raw_input(prompt2)
-	prompt3 = 'Please input 3rd length'
+	prompt3 = 'Please input 3rd length\n'
 	c = raw_input(prompt3)
 
-	is_triangle(int(a), int(b), int(c)
+	is_triangle(int(a), int(b), int(c))
 
-# prompt_lengths()
-# prompt = 'please enter length'
-# a = raw_input(prompt)
-# print int(a)
-
-
+prompt_lengths()
